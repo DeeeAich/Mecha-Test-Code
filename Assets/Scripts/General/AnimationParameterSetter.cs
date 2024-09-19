ba212f935e3baf53953bb904ba99f5aea2706d7f
jacobbb61 <104024873+jacobbb61@users.noreply.github.com>
2024-09-19 15:22:31 +0100

    tank chassis animations

--- Assets/Scripts/General/AnimationParameterSetter.cs
+++ Assets/Scripts/General/AnimationParameterSetter.cs
@@ -6,14 +6,25 @@ using UnityEngine.Events;
 public class AnimationParameterSetter : MonoBehaviour
 {
     [SerializeField] private Animator animator;
-    
+    [SerializeField] private Animator secondAnimator;
+    [SerializeField] private bool isUsingSecondAnimator = false;
+
+
     public void SetBoolTrue(string name)
     {
         animator.SetBool(name,true);
+        if (isUsingSecondAnimator)
+        {
+            secondAnimator.SetBool(name, true);
+        }
     }
 
     public void SetBoolFalse(string name)
     {
         animator.SetBool(name, false);
+        if (isUsingSecondAnimator)
+        {
+            secondAnimator.SetBool(name, false);
+        }
     }
 }
