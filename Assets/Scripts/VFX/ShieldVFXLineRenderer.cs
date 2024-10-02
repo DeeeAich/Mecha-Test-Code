ec1c34f839fbbbf6de4999ca22689e5e28dd8c60
jacobbb61 <104024873+jacobbb61@users.noreply.github.com>
2024-10-02 15:19:57 +0100

    ui scale down

---
+++ Assets/Scripts/VFX/ShieldVFXLineRenderer.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldVFXLineRenderer : MonoBehaviour
+{
+    public LineRenderer lineRenderer;
+
+    public Transform lineStart;
+    public Transform point01;
+    public Transform point02;
+    public Transform point03;
+    public Transform lineEnd;
+
+    public Transform line01;
+    public Transform line02;
+    public Transform line03;
+
+    public bool setPos;
+    public float timeBetweenSet;
+    public float jitterDistance;
+
+    void Update()
+    {
+        SetLinePositionsBetweenStartEnd();
+
+        lineRenderer.SetPosition(0, lineStart.position);
+        lineRenderer.SetPosition(1, point01.position);
+        lineRenderer.SetPosition(2, point02.position);
+        lineRenderer.SetPosition(3, point03.position);
+        lineRenderer.SetPosition(4, lineEnd.position);
+
+
+        if (setPos)
+        {
+            setPos = false;
+            StartCoroutine(TimeBetweenSetPos());
+        }
+    }
+
+    void SetLinePositionsBetweenStartEnd()
+    {
+        line02.localPosition = GetCenterPoint(lineEnd.localPosition,lineStart.localPosition);
+        line01.localPosition = GetCenterPoint(line02.localPosition,lineStart.localPosition);
+        line03.localPosition = GetCenterPoint(lineEnd.localPosition,line02.localPosition);
+    }
+    public Vector3 GetCenterPoint(Vector3 A, Vector3 B)
+    {
+        Vector3 P = (B + A)/2;
+        return P;
+    }
+
+    void SetLinePointRandomLocalPosition(Transform point)
+    {
+        point.localPosition = new Vector3(
+            Random.Range(-jitterDistance, jitterDistance),
+            Random.Range(-jitterDistance, jitterDistance),
+            Random.Range(-jitterDistance, jitterDistance)
+            );
+    }
+
+    IEnumerator TimeBetweenSetPos()
+    {
+        SetLinePointRandomLocalPosition(point01);
+        SetLinePointRandomLocalPosition(point02);
+        SetLinePointRandomLocalPosition(point03);
+        yield return new WaitForSeconds(timeBetweenSet);
+ 
+        StartCoroutine(TimeBetweenSetPos());
+    }
+}
