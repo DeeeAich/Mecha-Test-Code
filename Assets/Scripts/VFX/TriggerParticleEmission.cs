6e1a206f76f8de828867549fcdb96af3a37ec977
jacobbb61 <mcconnelljacob61@gmail.com>
2024-10-16 21:44:34 +0100

    not so mini gun :3

--- Assets/Scripts/VFX/TriggerParticleEmission.cs
+++ Assets/Scripts/VFX/TriggerParticleEmission.cs
@@ -5,13 +5,22 @@ using UnityEngine;
 public class TriggerParticleEmission : MonoBehaviour
 {
     public ParticleSystem ps;
+    public ParticleSystem ps2nd;
     public int numberOfParticles = 1;
+    public int numberOfParticles2nd = 1;
 
     public void TriggerParticle()
     {
         if (ps != null && ps.gameObject.activeInHierarchy) 
         { 
             ps.Emit(numberOfParticles); 
+        }
+    }
+    public void Trigger2ndParticle()
+    {
+        if (ps2nd != null && ps2nd.gameObject.activeInHierarchy)
+        {
+            ps2nd.Emit(numberOfParticles2nd);
         }
     }
 }
