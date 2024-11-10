a65ec2b690cbefcebb013bd1c25a644061e2ee7c
Grumtom <Tomgrumley2002@gmail.com>
2024-11-10 19:58:54 +0000

    changes to gitignore and capture zone

--- Assets/Scripts/Level and Environmental/Objectives/Primary/CaptureZonesPrimaryObjective.cs
+++ Assets/Scripts/Level and Environmental/Objectives/Primary/CaptureZonesPrimaryObjective.cs
@@ -2,9 +2,13 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CaptureZonesPrimaryObjective : Objective
 {
+    [SerializeField] private Image progressBar;
+    private float totalProgress;
+    
     private CaptureZone[] zones;
     private int zonesCaptured;
 
@@ -18,10 +22,15 @@ public class CaptureZonesPrimaryObjective : Objective
         if (!isComplete)
         {
             zonesCaptured = 0;
+            totalProgress = 0;
+            
             for (int i = 0; i < zones.Length; i++)
             {
+                totalProgress += zones[i].captureProgress / zones.Length;
                 if (zones[i].isCaptured) zonesCaptured++;
             }
+
+            progressBar.fillAmount = totalProgress;
 
             if (zonesCaptured == zones.Length)
             {
