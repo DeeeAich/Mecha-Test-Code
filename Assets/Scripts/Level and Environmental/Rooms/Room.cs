8a7124b0ef4ac7cea4a9946135d22536a2e149bd
Grumtom <Tomgrumley2002@gmail.com>
2024-11-25 15:48:53 +0000

    fixed infinite enemy glitch

--- Assets/Scripts/Level and Environmental/Rooms/Room.cs
+++ Assets/Scripts/Level and Environmental/Rooms/Room.cs
@@ -139,7 +139,12 @@ public class Room : MonoBehaviour
                 exitDoors[i].UnlockDoor();
             }
             
-            Debug.Log("Finished Room: " + gameObject.name);
+            Debug.Log("Finished Room: " + LevelGenerator.instance.roomIndex);
+
+            for (int i = 0; i < waveSpawners.Length; i++)
+            {
+                waveSpawners[i].isComplete = true;
+            }
         }
     }
 }
