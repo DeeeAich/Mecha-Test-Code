c70279ace3fa54e6e12abc48f66120ef7eead8bc
jacobbb61 <mcconnelljacob61@gmail.com>
2024-12-13 20:15:15 +0000

    dmg number update, shield hp bar fix

--- Assets/Scripts/UI/EnemyDamageNumberSpawner.cs
+++ Assets/Scripts/UI/EnemyDamageNumberSpawner.cs
@@ -24,31 +24,34 @@ public class EnemyDamageNumberSpawner : MonoBehaviour
 
     public void SpawnDamageNumber(float damageAmount, bool isCritical = false)
     {
+        int damage = Mathf.FloorToInt(damageAmount);
+
         GameObject newDamageNumberInstance = Instantiate(damageNumberInstance, this.transform);
         newDamageNumberInstance.GetComponent<RectTransform>().anchoredPosition3D = Vector3.zero;
-        newDamageNumberInstance.GetComponent<TextMeshProUGUI>().text = damageAmount.ToString();
+        newDamageNumberInstance.GetComponent<Animator>().SetFloat("Horizontal", Random.Range(0f, 1f));
+        newDamageNumberInstance.GetComponent<TextMeshProUGUI>().text = damage.ToString();
         if (isCritical)
         {
          newDamageNumberInstance.GetComponent<TextMeshProUGUI>().color = critColor;
         }
 
-        if (damageAmount <= 10) { newDamageNumberInstance.GetComponent<TextMeshProUGUI>().fontSize = 0.4f; }
-        if (damageAmount <= 20 && damageAmount > 10) { newDamageNumberInstance.GetComponent<TextMeshProUGUI>().fontSize = 0.5f; }
-        if (damageAmount <= 30 && damageAmount > 20) { newDamageNumberInstance.GetComponent<TextMeshProUGUI>().fontSize = 0.6f; }
-        if (damageAmount <= 50 && damageAmount > 30) { newDamageNumberInstance.GetComponent<TextMeshProUGUI>().fontSize = 0.7f; }
-        if (damageAmount <= 80 && damageAmount > 50) { newDamageNumberInstance.GetComponent<TextMeshProUGUI>().fontSize = 0.8f; }
-        if (damageAmount <= 100 && damageAmount > 80) { newDamageNumberInstance.GetComponent<TextMeshProUGUI>().fontSize = 1f; }
-        if (damageAmount <= 150 && damageAmount > 100) { newDamageNumberInstance.GetComponent<TextMeshProUGUI>().fontSize = 1.1f; }
-        if (damageAmount <= 200 && damageAmount > 150) { newDamageNumberInstance.GetComponent<TextMeshProUGUI>().fontSize = 1.2f; }
-        if (damageAmount <= 300 && damageAmount > 200) { newDamageNumberInstance.GetComponent<TextMeshProUGUI>().fontSize = 1.3f; }
-        if (damageAmount <= 400 && damageAmount > 300) { newDamageNumberInstance.GetComponent<TextMeshProUGUI>().fontSize = 1.4f; }
-        if (damageAmount <= 550 && damageAmount > 400) { newDamageNumberInstance.GetComponent<TextMeshProUGUI>().fontSize = 1.5f; }
-        if (damageAmount <= 700 && damageAmount > 550) { newDamageNumberInstance.GetComponent<TextMeshProUGUI>().fontSize = 1.7f; }
-        if (damageAmount <= 800 && damageAmount > 700) { newDamageNumberInstance.GetComponent<TextMeshProUGUI>().fontSize = 1.8f; }
-        if (damageAmount <= 900 && damageAmount > 800) { newDamageNumberInstance.GetComponent<TextMeshProUGUI>().fontSize = 1.9f; }
-        if (damageAmount <= 1000 && damageAmount > 900) { newDamageNumberInstance.GetComponent<TextMeshProUGUI>().fontSize = 2f; }
-        if (damageAmount > 1000) { newDamageNumberInstance.GetComponent<TextMeshProUGUI>().fontSize = 2f; }
+        if (damage <= 10) { newDamageNumberInstance.GetComponent<TextMeshProUGUI>().fontSize = 0.4f; }
+        if (damage <= 20 && damage > 10) { newDamageNumberInstance.GetComponent<TextMeshProUGUI>().fontSize = 0.5f; }
+        if (damage <= 30 && damage > 20) { newDamageNumberInstance.GetComponent<TextMeshProUGUI>().fontSize = 0.6f; }
+        if (damage <= 50 && damage > 30) { newDamageNumberInstance.GetComponent<TextMeshProUGUI>().fontSize = 0.7f; }
+        if (damage <= 80 && damage > 50) { newDamageNumberInstance.GetComponent<TextMeshProUGUI>().fontSize = 0.8f; }
+        if (damage <= 100 && damage > 80) { newDamageNumberInstance.GetComponent<TextMeshProUGUI>().fontSize = 1f; }
+        if (damage <= 150 && damage > 100) { newDamageNumberInstance.GetComponent<TextMeshProUGUI>().fontSize = 1.1f; }
+        if (damage <= 200 && damage > 150) { newDamageNumberInstance.GetComponent<TextMeshProUGUI>().fontSize = 1.2f; }
+        if (damage <= 300 && damage > 200) { newDamageNumberInstance.GetComponent<TextMeshProUGUI>().fontSize = 1.3f; }
+        if (damage <= 400 && damage > 300) { newDamageNumberInstance.GetComponent<TextMeshProUGUI>().fontSize = 1.4f; }
+        if (damage <= 550 && damage > 400) { newDamageNumberInstance.GetComponent<TextMeshProUGUI>().fontSize = 1.5f; }
+        if (damage <= 700 && damage > 550) { newDamageNumberInstance.GetComponent<TextMeshProUGUI>().fontSize = 1.7f; }
+        if (damage <= 800 && damage > 700) { newDamageNumberInstance.GetComponent<TextMeshProUGUI>().fontSize = 1.8f; }
+        if (damage <= 900 && damage > 800) { newDamageNumberInstance.GetComponent<TextMeshProUGUI>().fontSize = 1.9f; }
+        if (damage <= 1000 && damage > 900) { newDamageNumberInstance.GetComponent<TextMeshProUGUI>().fontSize = 2f; }
+        if (damage > 1000) { newDamageNumberInstance.GetComponent<TextMeshProUGUI>().fontSize = 2f; }
 
-        Destroy(newDamageNumberInstance, 1f);
+        Destroy(newDamageNumberInstance, 0.75f);
     }
 }
