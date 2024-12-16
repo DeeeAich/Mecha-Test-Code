a6c602eb05781aea5f0e7a276f35b0f7fd169204
jacobbb61 <mcconnelljacob61@gmail.com>
2024-12-16 13:52:44 +0000

    fmod stuff and amv test

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -3,12 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using FMODUnity;
 
+
+public enum MusicState
+{
+    idle,
+    combat,
+    muffled,
+    stopImmediately,
+    fadeOut
+}
+
 public class AudioManager : MonoBehaviour
 {
     public static AudioManager instance;
 
+    [Header("Music")]
+    public MusicState musicState;
+    public EventReference music;
 
-    public EventReference musicTest;
+    [Header("Ambience")]
+    public EventReference ambience;
 
 
     private void Awake()
@@ -21,12 +35,18 @@ public class AudioManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
-       RuntimeManager.PlayOneShot(musicTest);
+       RuntimeManager.PlayOneShot(music);
+       RuntimeManager.PlayOneShot(ambience);
 
     }
 
     public void PlayOneShotSFX(EventReference sound, Vector3 worldPos)
     {
         RuntimeManager.PlayOneShot(sound, worldPos);
+    }
+
+    public void ChangeMusicState(MusicState newState)
+    {
+        
     }
 }
