dec1e7f5ea0f0fa5a32d723ab88ff826820b06d7
Grumtom <Tomgrumley2002@gmail.com>
2025-02-07 12:33:13 +0000

    hid damage numbers in inspector

--- Assets/Scripts/UI/EnemyDamageNumberSpawner.cs
+++ Assets/Scripts/UI/EnemyDamageNumberSpawner.cs
@@ -27,6 +27,7 @@ public class EnemyDamageNumberSpawner : MonoBehaviour
         int damage = Mathf.FloorToInt(damageAmount);
 
         GameObject newDamageNumberInstance = Instantiate(damageNumberInstance, null);
+        newDamageNumberInstance.hideFlags = HideFlags.HideInHierarchy;
         newDamageNumberInstance.GetComponent<RectTransform>().anchoredPosition3D = transform.position;
         newDamageNumberInstance.GetComponent<Animator>().SetFloat("Horizontal", Random.Range(0f, 1f));
         newDamageNumberInstance.GetComponent<TextMeshProUGUI>().text = damage.ToString();
