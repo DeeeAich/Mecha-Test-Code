f91522d570c77d2d1aca6c34a86319eb917801d4
Grumtom <Tomgrumley2002@gmail.com>
2025-02-13 19:33:22 +0000

    Fixed pause menu

--- Assets/Scripts/Menus/pauseMenu.cs
+++ Assets/Scripts/Menus/pauseMenu.cs
@@ -8,41 +8,54 @@ using UnityEngine.SceneManagement;
 
 public class pauseMenu : MonoBehaviour
 {
+    public bool canPause = true;
+    
     [SerializeField] private GameObject menu;
+
+    private InputAction pauseAction;
+
+    private void Start()
+    {
+        pauseAction = PlayerBody.PlayBody().GetComponent<PlayerInput>().actions["Pause"];
+        pauseAction.performed += onPauseButtonPressed;
+    }
 
     public void PauseGame()
     {
-        DevKitCheats devkit = FindObjectOfType<DevKitCheats>();
-        if (devkit != null && devkit.devkitCheatMenu.activeSelf) // gets rid of devkit menu
+        if (canPause)
         {
-            devkit.devkitCheatMenu.SetActive(false);
-        }
+            DevKitCheats devkit = FindObjectOfType<DevKitCheats>();
+            if (devkit != null && devkit.devkitCheatMenu.activeSelf) // gets rid of devkit menu
+            {
+                devkit.devkitCheatMenu.SetActive(false);
+            }
 
-        if (Time.timeScale == 1)
-        {
-            Time.timeScale = 0;
-            PlayerBody.PlayBody().StopParts(false, false);
-            menu.SetActive(true);
-        }
-        else if (Time.timeScale == 0)
-        {
-            Time.timeScale = 1;
-            PlayerBody.PlayBody().StopParts(true, true);
-            menu.SetActive(false);
+            if (Time.timeScale == 1)
+            {
+                Time.timeScale = 0;
+                PlayerBody.PlayBody().StopParts(false, false);
+                menu.SetActive(true);
+            }
+            else if (Time.timeScale == 0)
+            {
+                Time.timeScale = 1;
+                PlayerBody.PlayBody().StopParts(true, true);
+                menu.SetActive(false);
+            }
         }
     }
 
     public void onPauseButtonPressed(InputAction.CallbackContext context)
     {
-        if (context.started)
-        {
-            PauseGame();
-        }
+        print("Pause button");
+        PauseGame();
     }
 
     public void Reset()
     {
-        Scene scene = SceneManager.GetActiveScene();
-        SceneManager.LoadScene(scene.name);
+        Debug.Log("Resetting Game");
+        pauseAction.performed -= onPauseButtonPressed;
+        PauseGame();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
