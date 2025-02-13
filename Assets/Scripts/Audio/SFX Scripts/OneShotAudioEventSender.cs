345a11335a3b15707d5f00b5672ca05afe8e4f0d
jacobbb61 <mcconnelljacob61@gmail.com>
2025-02-13 19:07:26 +0000

    shader revert, bits, lots of bits

--- Assets/Scripts/Audio/SFX Scripts/OneShotAudioEventSender.cs
+++ Assets/Scripts/Audio/SFX Scripts/OneShotAudioEventSender.cs
@@ -6,8 +6,13 @@ using FMODUnity;
 public class OneShotAudioEventSender : MonoBehaviour
 {
     public List<EventReference> sound;
-
+    public string parameterName = "none";
     public void PlayOneShot(int soundNumber)
+    {
+       AudioManager.instance.PlayOneShotSFX(sound[soundNumber], this.transform.position);
+        
+    }
+    public void PlayOneShotWithParameter(int soundNumber)
     {
         AudioManager.instance.PlayOneShotSFX(sound[soundNumber], this.transform.position);
     }
