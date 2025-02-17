d805834845f58520557d0ec8ef5950aaf57050ea
Grumtom <Tomgrumley2002@gmail.com>
2025-02-17 21:05:59 +0000

    Started on inventory and made room testing function

---
+++ Assets/Scripts/Menus/InventoryManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryManager : MonoBehaviour
+{
+    public Button[] weaponLeftChipsButtons;
+    public Button[] weaponRightChipsButtons;
+
+    public void CheckInventory()
+    {
+        
+    }
+}
