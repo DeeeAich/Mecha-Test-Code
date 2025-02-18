74abae65d00b91f2568c068d7cad7b453fdad856
Grumtom <Tomgrumley2002@gmail.com>
2025-02-18 15:13:21 +0000

    Added inventory chip display

--- Assets/Scripts/Menus/InventoryManager.cs
+++ Assets/Scripts/Menus/InventoryManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -17,9 +18,33 @@ public class InventoryManager : MonoBehaviour
     public Button[] weaponLeftChipsButtons;
     public Button[] weaponRightChipsButtons;
 
-    private void Start()
+    [Header("Inspection Card")] 
+    [SerializeField] private GameObject inspectionCard;
+    [SerializeField] private TMP_Text inspectionCardTitle;
+    [SerializeField] private TMP_Text inspectionCardDescription;
+    [SerializeField] private Image inspectionCardImage;
+    
+    private void Awake()
     {
-        FindObjectOfType<pauseMenu>().onPause.AddListener(UpdateInventory);
+        FindObjectOfType<pauseMenu>().onPause.AddListener(delegate { UpdateInventory(); });
+
+        for (int i = 0; i < weaponLeftChipsButtons.Length; i++)
+        {
+            int index = i;
+            weaponLeftChipsButtons[i].onClick.AddListener(delegate { OpenInspectionCard(0, index); });
+        }
+
+        for (int i = 0; i < weaponRightChipsButtons.Length; i++)
+        {
+            int index = i;
+            weaponRightChipsButtons[i].onClick.AddListener(delegate { OpenInspectionCard(1, index); });
+        }
+
+        for (int i = 0; i < bodyChipsButtons.Length; i++)
+        {
+            int index = i;
+            bodyChipsButtons[i].onClick.AddListener(delegate { OpenInspectionCard(2, index); });
+        }
     }
 
     private void Update()
@@ -33,6 +58,10 @@ public class InventoryManager : MonoBehaviour
 
     public void UpdateInventory()
     {
+        print("Updating Inventory");
+        
+        inspectionCard.SetActive(false);
+        
         List<WeaponChip> leftMods = PlayerBody.PlayBody().GetComponent<PlayerWeaponControl>().leftMods;
         List<WeaponChip> rightMods = PlayerBody.PlayBody().GetComponent<PlayerWeaponControl>().rightMods;
 
@@ -63,8 +92,7 @@ public class InventoryManager : MonoBehaviour
             }
         }
 
-        List<BodyChip> bodyChips = new List<BodyChip>();
-        // = PlayerBody.PlayBody().myMods;
+        List<Chip> bodyChips = PlayerBody.PlayBody().chipsInserted;
 
         for (int i = 0; i < bodyChipsImages.Count; i++)
         {
@@ -78,5 +106,58 @@ public class InventoryManager : MonoBehaviour
                 bodyChipsImages[i].enabled = false;
             }
         }
+    }
+
+    public void OpenInspectionCard(int section, int index)
+    {
+        inspectionCard.SetActive(false);
+        Chip chip = null;
+        
+        switch (section)
+        {
+            case 0: // left weapon
+                if (index >= PlayerBody.PlayBody().GetComponent<PlayerWeaponControl>().leftMods.Count)
+                {
+                    return;
+                }
+                
+                inspectionCard.transform.position = WeaponsLeftChipsImages[index].transform.position;
+                chip = PlayerBody.PlayBody().GetComponent<PlayerWeaponControl>().leftMods[index];
+                break;
+            
+            case 1: // right weapon
+                if (index >= PlayerBody.PlayBody().GetComponent<PlayerWeaponControl>().rightMods.Count)
+                {
+                    return;
+                }
+                
+                inspectionCard.transform.position = WeaponsRightChipsImages[index].transform.position;
+                chip = PlayerBody.PlayBody().GetComponent<PlayerWeaponControl>().rightMods[index];
+                break;
+            
+            case 2: // body chips
+                if (index >= PlayerBody.PlayBody().chipsInserted.Count)
+                {
+                    return;
+                }
+                
+                inspectionCard.transform.position = bodyChipsImages[index].transform.position;
+                chip = PlayerBody.PlayBody().chipsInserted[index];
+                break;
+        }
+
+        if (chip != null)
+        {
+            inspectionCardImage.sprite = chip.chipImage;
+            inspectionCardDescription.text = chip.chipDes;
+            inspectionCardTitle.text = chip.chipName;
+        }
+
+        inspectionCard.SetActive(true);
+    }
+
+    public void CloseInspectionCard()
+    {
+        
     }
 }
