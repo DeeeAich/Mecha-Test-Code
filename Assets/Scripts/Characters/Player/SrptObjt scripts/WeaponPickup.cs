7a690e2001878dec8e4aba3ebde68c042e8fc508
DeeAich <donnachadavylohan@gmail.com>
2025-02-22 17:52:32 +0000

    Player Asset for weapon creation

--- Assets/Scripts/Characters/Player/SrptObjt scripts/WeaponPickup.cs
+++ Assets/Scripts/Characters/Player/SrptObjt scripts/WeaponPickup.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "newWeapon", menuName = "Player/Weapon")]
 public class WeaponPickup : PlayerPickup
 {
     public GameObject weaponPrefab;
