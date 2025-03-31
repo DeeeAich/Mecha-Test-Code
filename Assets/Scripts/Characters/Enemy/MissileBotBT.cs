e79ebd20ab2ce7574c1e11a867b6c76866cb7095
Meemitone <99894591+Meemitone@users.noreply.github.com>
2025-03-31 22:56:09 +0100

    Missile Recovered

---
+++ Assets/Scripts/Characters/Enemy/MissileBotBT.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AITree;
+
+public class MissileBotBT : BehaviourTree
+{
+    public float approachDist;
+    public float peekStepSize = 2f;
+    internal override void Awake()
+    {
+        base.Awake();
+        AddOrOverwrite("player", player);
+        root = new RootNode(this,
+                   new Fallback(
+                       //new FindTarget("targetType", "target"),
+                       new HasLineOfSight("player", PositionStoreType.GAMEOBJECT),
+                       new Sequence(new FindLineOfSightPosition("LOSPOS", 5, peekStepSize, "player", PositionStoreType.GAMEOBJECT), new Approach("LOSPOS", 0.1f, PositionStoreType.VECTOR3)),
+                       new ApproachUntilLineOfSight("player", approachDist)
+                )
+            );
+    }
+
+    override internal void FixedUpdate()
+    {
+        base.FixedUpdate();
+    }
+}
---
+++ Assets/Scripts/Characters/Enemy/MissileMotion.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class MissileMotion : MoveProjectile
+{
+    float speed;
+    float timeto180 = 5f;
+    GameObject target;
+    private void Awake()
+    {
+        speed = localVelocity.magnitude;
+    }
+
+    internal override void FixedUpdate()
+    {
+        if(target == null)
+        {
+            target = PlayerBody.Instance().gameObject;
+        }
+        transform.position += transform.forward * speed * Time.fixedDeltaTime;
+        Vector3 targetPos = target.transform.position;
+        targetPos.y = transform.position.y;
+        Quaternion targetRot = Quaternion.LookRotation(targetPos - transform.position, Vector3.up);
+        float rotPerUpdate = (Time.fixedDeltaTime / timeto180) * 180f;
+        float totalAngle = Quaternion.Angle(targetRot, transform.rotation);
+        float t = rotPerUpdate / totalAngle;
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRot, t);
+    }
+
+    public override void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent(out Health health))
+        {
+            if (damageableEntities.Contains(health.entityType))
+            {
+                health.TakeDamage(damage);
+
+                pierceCount--;
+
+                if (pierceCount == 0)
+                {
+                    Destroy(gameObject);
+                    //transform.GetComponentInChildren<Animator>().SetTrigger("impact");
+                }/*
+                else
+                {
+                    if (pierceCount > 0)
+                    {
+                        pierceCount--;
+                        if (pierceCount <= 0)
+                        {
+                            Destroy(gameObject, 1f);
+                            globalVelocity = Vector3.zero;
+                            transform.GetComponentInChildren<Animator>().SetTrigger("impact");
+                        }
+                    }
+                }*/
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+            //transform.GetComponentInChildren<Animator>().SetTrigger("impact");
+
+        }
+    }
+}
+
