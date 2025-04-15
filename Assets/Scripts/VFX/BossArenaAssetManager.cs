01687b0fbf10a3c70aab43803c242fe17d59deab
jacobbb61 <mcconnelljacob61@gmail.com>
2025-04-15 20:32:43 +0100

    boss arena ready for implementation, also deleted unused emission and metallic textures

--- Assets/Scripts/VFX/BossArenaAssetManager.cs
+++ Assets/Scripts/VFX/BossArenaAssetManager.cs
@@ -7,6 +7,9 @@ public class BossArenaAssetManager : MonoBehaviour
     public Animator phaseOneArena;
     public Animator fallingAnim;
 
+    public GameObject cameraPivot;
+    public RoomUpperLevelToggle roomUpperLevelToggle;
+
     public bool test;
     void Update()
     {
@@ -19,6 +22,16 @@ public class BossArenaAssetManager : MonoBehaviour
 
     public void StartTransition()
     {
+        if(roomUpperLevelToggle.roomAngle == 45)
+        {
+            cameraPivot.transform.localRotation = Quaternion.Euler(0, 0, 0);
+        }
+        else 
+        {
+            cameraPivot.transform.localRotation = Quaternion.Euler(0, -90, 0);
+        }
+
+
         phaseOneArena.SetTrigger("Transition");
         fallingAnim.SetTrigger("Transition");
     }
