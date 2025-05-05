87961f4f4767de8b9224426ee89389ba4567b16b
Grumtom <Tomgrumley2002@gmail.com>
2025-05-05 15:25:01 +0100

    fixed sniper lazer and boss navmesh

--- Assets/Scripts/VFX/Line Renderer Stuff/SniperLaserRenderer.cs
+++ Assets/Scripts/VFX/Line Renderer Stuff/SniperLaserRenderer.cs
@@ -11,14 +11,19 @@ public class SniperLaserRenderer : MonoBehaviour
     void FixedUpdate()
     {
 
+        lineRenderer.SetPosition(0, transform.position);
+        
         RaycastHit hit;
         // Does the ray intersect any objects excluding the player layer
         if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask))
-
         {
             Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
-            lineRenderer.SetPosition(0, transform.position);
+     
             lineRenderer.SetPosition(1, hit.point);
+        }
+        else
+        {
+            lineRenderer.SetPosition(1, transform.position + transform.forward * 100);
         }
     }
 }
