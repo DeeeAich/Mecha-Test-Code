e4f3f3be0645abaf133a85aa43afe5def339dabe
Grumtom <Tomgrumley2002@gmail.com>
2025-05-05 14:15:54 +0100

    tesla zap vfx

---
+++ Assets/Scripts/VFX/TeslaVFX.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeslaVFX : MonoBehaviour
+{
+    [SerializeField] private GameObject zapFFXPrefab;
+
+    public void ZapObject(GameObject target)
+    {
+        GameObject zap = Instantiate(zapFFXPrefab, transform);
+        zap.transform.localPosition = Vector3.zero;
+        zap.GetComponent<ShieldVFXLineRendererManager>().shieldedTarget = gameObject;
+    }
+}
