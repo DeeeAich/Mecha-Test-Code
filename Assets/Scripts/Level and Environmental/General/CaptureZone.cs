f7c859b660ca10bff6369285fe93d5b7be4f24e9
jacobbb61 <mcconnelljacob61@gmail.com>
2025-05-06 17:36:53 +0100

    cutscene fix, capture point, tesla bot, missile hit vfx, missile not flying upwards, main menu bits

--- Assets/Scripts/Level and Environmental/General/CaptureZone.cs
+++ Assets/Scripts/Level and Environmental/General/CaptureZone.cs
@@ -3,33 +3,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using TMPro;
+using UnityEngine.UI;
 
 public class CaptureZone : MonoBehaviour
 {
     public UnityEvent onCapture;
+
     public bool isCaptured;
     public float captureProgress;
     public float captureProgressPerSecond = 0.01f;
+    public Image capturePorgressImage;
 
     public bool playerPresent;
     public int enemiesPresent;
 
+    [SerializeField] private Animator animator;
     [SerializeField] private LayerMask characterLayers;
-    
+
 
     private void FixedUpdate()
     {
+        animator.SetBool("Charging", playerPresent);
+        animator.SetBool("EnemyOnPoint", enemiesPresent > 0);
+
         if (playerPresent && enemiesPresent == 0)
         {
             captureProgress += captureProgressPerSecond * Time.fixedDeltaTime;
+            capturePorgressImage.fillAmount = captureProgress;
         }
 
-        if (captureProgress >= 1)
+        if (captureProgress >= 1 && !isCaptured)
         {
             isCaptured = true;
+            animator.SetTrigger("Complete");
             onCapture.Invoke();
         }
-        
+
         enemiesPresent = 0;
         playerPresent = false;
     }
