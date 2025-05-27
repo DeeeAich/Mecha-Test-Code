0d0d98711f2888102b4bf1e7e85f1b0974f7b8cc
Meemitone <99894591+Meemitone@users.noreply.github.com>
2025-05-27 11:34:41 +0100

    Overseer Look Lock Jitter Removed

--- Assets/Scripts/Characters/Enemy/Boss/OverseerWeaponsLook.cs
+++ Assets/Scripts/Characters/Enemy/Boss/OverseerWeaponsLook.cs
@@ -42,7 +42,7 @@ public class OverseerWeaponsLook : MonoBehaviour
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
         if(lookTarget == null)
         {
