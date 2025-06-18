04bca9e68da12e766e09091b46b1dc4ae5894c90
jacobbb61 <mcconnelljacob61@gmail.com>
2025-06-18 17:06:41 +0100

    art experimenting
    lots of layer changings and material edits

--- Assets/Scripts/Level and Environmental/Rooms/IndoorOutdoorTransition.cs
+++ Assets/Scripts/Level and Environmental/Rooms/IndoorOutdoorTransition.cs
@@ -4,31 +4,106 @@ using UnityEngine;
 
 public class IndoorOutdoorTransition : MonoBehaviour
 {
-    public List<GameObject> outdoorObjects;
-    public List<GameObject> indoorObjects;
+    [Header("Meshes")]
+    public MeshRenderer[] myMeshRenderer;
+    public float myTransparency = 1f;
 
-    
-
-    public void TransitionOutdoors()
+    [Header("Lights")]
+    public Light[] myLights;
+    public LightInfo[] myLightInfo;
+    public struct LightInfo
     {
-        for (int i = 0; i < outdoorObjects.Count; i++)
-        {
-            outdoorObjects[i].SetActive(true);
-        }
-        for (int i = 0; i < outdoorObjects.Count; i++)
-        {
-            indoorObjects[i].SetActive(false);
-        }
+        public Light myLight;
+        public float myInitialIntensity;
     }
-    public void TransitionIndoors()
+
+
+    [Header("Room State")]
+    public bool isTransparent;
+    public bool fadingIn;
+    public bool fadingOut;
+    public float fadeSpeed = 5f;
+
+    public float lerpTimer;
+
+    private void Start()
     {
-        for (int i = 0; i < outdoorObjects.Count; i++)
+        myMeshRenderer = GetComponentsInChildren<MeshRenderer>();
+        myLights = GetComponentsInChildren<Light>();
+
+        myLightInfo = new LightInfo[myLights.Length];
+        for (int i = 0; i < myLights.Length; i++)
         {
-            outdoorObjects[i].SetActive(false);
+            myLightInfo[i].myLight = myLights[i];
+            myLightInfo[i].myInitialIntensity = myLights[i].intensity;
+
+            if (isTransparent) { myLightInfo[i].myLight.intensity = 0; }
         }
-        for (int i = 0; i < outdoorObjects.Count; i++)
+
+        if (isTransparent)
         {
-            indoorObjects[i].SetActive(true);
+            myTransparency = 0;
+        }
+        else
+        {
+            myTransparency = 1;
+        }
+        FadeMyMaterials();
+    }
+
+    public void FixedUpdate()
+    {
+        lerpTimer += Time.fixedDeltaTime * fadeSpeed;
+
+        if (fadingIn)
+        {
+            myTransparency = Mathf.Lerp(0, 1, lerpTimer);
+            FadeMyMaterials();
+
+            for (int i = 0; i < myLightInfo.Length; i++)
+            {
+                myLightInfo[i].myLight.intensity = Mathf.Lerp(0, myLightInfo[i].myInitialIntensity, lerpTimer);       
+            }
+
+            if (myTransparency >= 1)
+            {
+                myTransparency = 1;
+                isTransparent = false;
+                fadingIn = false;
+            }
+        }
+        
+        if (fadingOut)
+        {
+            myTransparency = Mathf.Lerp(1, 0, lerpTimer);
+            FadeMyMaterials();
+
+            for (int i = 0; i < myLightInfo.Length; i++)
+            {
+                myLightInfo[i].myLight.intensity = Mathf.Lerp(myLightInfo[i].myInitialIntensity, 0, lerpTimer);
+            }
+
+            if (myTransparency <= 0)
+            {
+                myTransparency = 0;
+                isTransparent = true;
+                fadingOut = false;
+            }
+        }
+
+    }
+
+    public void TransitionRoom()
+    {
+        lerpTimer = 0;
+        if (!isTransparent) { fadingOut = true; }
+        else { fadingIn = true; }
+    }
+    public void FadeMyMaterials()
+    {
+        for (int i = 0; i < myMeshRenderer.Length; i++)
+        {
+            myMeshRenderer[i].material.SetFloat("_Transparency", myTransparency);
         }
     }
 }
